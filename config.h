243715083bf95005a627c7ab94c3e451029994fb
cexh <cexh@proton.me>
2024-10-11 15:49:30 -0300

    vim bindings add to config.h

--- config.h
+++ config.h
@@ -10,20 +10,23 @@ static const unsigned int default_speed = 500;
 /* changes the speed of the mouse pointer */
 static SpeedBinding speed_bindings[] = {
     /* key             speed */  
-    { XK_Super_L,      3000 },
-    { XK_Alt_L,        1500 },
-    { XK_a,            100  },
-    { XK_Control_L,    10   },
+    { XK_s,            1500 },
+    { XK_d,            100  },
+    { XK_x,            10   },
 };
 
 /* moves the mouse pointer
  * you can also add any other direction (e.g. diagonals) */
 static MoveBinding move_bindings[] = {
     /* key         x      y */
-    { XK_j,        -1,     0 },
+    { XK_h,        -1,     0 },
+    { XK_j,         0,     1 },
+    { XK_k,         0,    -1 },
     { XK_l,         1,     0 },
-    { XK_i,         0,    -1 },
-    { XK_k,         0,     1 },
+    { XK_u,        -1,    -1 },
+    { XK_i,         1,    -1 },
+    { XK_c,        -1,     1 },
+    { XK_m,         1,     1 },
 };
 
 /* 1: left
@@ -31,32 +34,34 @@ static MoveBinding move_bindings[] = {
  * 3: right */
 static ClickBinding click_bindings[] = {
     /* key         button */  
-    { XK_space,    1 },
     { XK_f,        1 },
-    { XK_d,        2 },
-    { XK_s,        3 },
+    { XK_g,        3 },
+    { XK_a,        2 },
 };
 
 /* scrolls up, down, left and right
  * a higher value scrolls faster */
 static ScrollBinding scroll_bindings[] = {
     /* key        x      y */
-    { XK_n,        0 ,    25 },
-    { XK_p,        0 ,   -25 },
-    { XK_plus,     0 ,    80 },
-    { XK_minus,    0 ,   -80 },
-    { XK_h,        25,    0  },
-    { XK_g,       -25,    0  },
+    { XK_e,        0 ,    25 },
+    { XK_w,        0 ,   -25 },
+    { XK_p,        25,    0  },
+    { XK_o,       -25,    0  },
 };
 
 /* executes shell commands */
 static ShellBinding shell_bindings[] = {
     /* key         command */  
-    { XK_b,        "wmctrl -a firefox" },
-    { XK_0,        "xdotool mousemove 0 0" },
+    { XK_t,        "" },
+    { XK_b,        "" },
+    { XK_r,        "" },
+    { XK_y,        "" },
+    { XK_v,        "" },
+    { XK_n,        "" },
+    { XK_z,        "" },
 };
 
 /* exits on key release which allows click and exit with one key */
 static KeySym exit_keys[] = {
-    XK_Escape, XK_q, XK_space
+    XK_q
 };
